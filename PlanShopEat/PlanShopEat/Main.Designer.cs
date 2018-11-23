b011f789cbc3894a8d86fe261b5e33df1f8eaccd
elbons <trentonsnoble@gmail.com>
2018-11-22 20:23:12 -0600

    added and named weekly menun controls; no formatting logic yet

--- PlanShopEat/PlanShopEat/Main.Designer.cs
+++ PlanShopEat/PlanShopEat/Main.Designer.cs
@@ -32,10 +32,26 @@
             this.labelFilter = new System.Windows.Forms.Label();
             this.textBoxFilter = new System.Windows.Forms.TextBox();
             this.groupBoxDishes = new System.Windows.Forms.GroupBox();
-            this.comboBoxCategory = new System.Windows.Forms.ComboBox();
             this.labelCategory = new System.Windows.Forms.Label();
+            this.comboBoxCategory = new System.Windows.Forms.ComboBox();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBoxWeekMenu = new System.Windows.Forms.GroupBox();
+            this.listBoxSunday = new System.Windows.Forms.ListBox();
+            this.labelSunday = new System.Windows.Forms.Label();
+            this.labelSaturday = new System.Windows.Forms.Label();
+            this.listBoxSaturday = new System.Windows.Forms.ListBox();
+            this.labelFriday = new System.Windows.Forms.Label();
+            this.listBoxFriday = new System.Windows.Forms.ListBox();
+            this.labelThurday = new System.Windows.Forms.Label();
+            this.listBoxThurday = new System.Windows.Forms.ListBox();
+            this.labelWednesday = new System.Windows.Forms.Label();
+            this.listBoxWednesday = new System.Windows.Forms.ListBox();
+            this.labelTuesday = new System.Windows.Forms.Label();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.labelMonday = new System.Windows.Forms.Label();
+            this.listBoxMonday = new System.Windows.Forms.ListBox();
             this.groupBoxDishes.SuspendLayout();
+            this.groupBoxWeekMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // listBoxFoodChoices
@@ -46,7 +62,7 @@
             this.listBoxFoodChoices.Location = new System.Drawing.Point(13, 56);
             this.listBoxFoodChoices.Name = "listBoxFoodChoices";
             this.listBoxFoodChoices.Size = new System.Drawing.Size(280, 420);
-            this.listBoxFoodChoices.TabIndex = 0;            
+            this.listBoxFoodChoices.TabIndex = 0;
             // 
             // labelFilter
             // 
@@ -82,15 +98,6 @@
             this.groupBoxDishes.TabStop = false;
             this.groupBoxDishes.Text = "Dishes";
             // 
-            // comboBoxCategory
-            // 
-            this.comboBoxCategory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.comboBoxCategory.FormattingEnabled = true;
-            this.comboBoxCategory.Location = new System.Drawing.Point(315, 76);
-            this.comboBoxCategory.Name = "comboBoxCategory";
-            this.comboBoxCategory.Size = new System.Drawing.Size(181, 24);
-            this.comboBoxCategory.TabIndex = 4;
-            // 
             // labelCategory
             // 
             this.labelCategory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
@@ -101,17 +108,178 @@
             this.labelCategory.TabIndex = 5;
             this.labelCategory.Text = "Category";
             // 
+            // comboBoxCategory
+            // 
+            this.comboBoxCategory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.comboBoxCategory.FormattingEnabled = true;
+            this.comboBoxCategory.Location = new System.Drawing.Point(315, 76);
+            this.comboBoxCategory.Name = "comboBoxCategory";
+            this.comboBoxCategory.Size = new System.Drawing.Size(181, 24);
+            this.comboBoxCategory.TabIndex = 4;
+            // 
+            // groupBoxWeekMenu
+            // 
+            this.groupBoxWeekMenu.Controls.Add(this.labelMonday);
+            this.groupBoxWeekMenu.Controls.Add(this.listBoxMonday);
+            this.groupBoxWeekMenu.Controls.Add(this.labelTuesday);
+            this.groupBoxWeekMenu.Controls.Add(this.listBox5);
+            this.groupBoxWeekMenu.Controls.Add(this.labelWednesday);
+            this.groupBoxWeekMenu.Controls.Add(this.listBoxWednesday);
+            this.groupBoxWeekMenu.Controls.Add(this.labelThurday);
+            this.groupBoxWeekMenu.Controls.Add(this.listBoxThurday);
+            this.groupBoxWeekMenu.Controls.Add(this.labelFriday);
+            this.groupBoxWeekMenu.Controls.Add(this.listBoxFriday);
+            this.groupBoxWeekMenu.Controls.Add(this.labelSaturday);
+            this.groupBoxWeekMenu.Controls.Add(this.listBoxSaturday);
+            this.groupBoxWeekMenu.Controls.Add(this.labelSunday);
+            this.groupBoxWeekMenu.Controls.Add(this.listBoxSunday);
+            this.groupBoxWeekMenu.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxWeekMenu.Name = "groupBoxWeekMenu";
+            this.groupBoxWeekMenu.Size = new System.Drawing.Size(402, 492);
+            this.groupBoxWeekMenu.TabIndex = 5;
+            this.groupBoxWeekMenu.TabStop = false;
+            this.groupBoxWeekMenu.Text = "Weekly Menu";
+            // 
+            // listBoxSunday
+            // 
+            this.listBoxSunday.FormattingEnabled = true;
+            this.listBoxSunday.ItemHeight = 16;
+            this.listBoxSunday.Location = new System.Drawing.Point(6, 56);
+            this.listBoxSunday.Name = "listBoxSunday";
+            this.listBoxSunday.Size = new System.Drawing.Size(120, 84);
+            this.listBoxSunday.TabIndex = 0;
+            // 
+            // labelSunday
+            // 
+            this.labelSunday.AutoSize = true;
+            this.labelSunday.Location = new System.Drawing.Point(7, 30);
+            this.labelSunday.Name = "labelSunday";
+            this.labelSunday.Size = new System.Drawing.Size(56, 17);
+            this.labelSunday.TabIndex = 1;
+            this.labelSunday.Text = "Sunday";
+            // 
+            // labelSaturday
+            // 
+            this.labelSaturday.AutoSize = true;
+            this.labelSaturday.Location = new System.Drawing.Point(167, 347);
+            this.labelSaturday.Name = "labelSaturday";
+            this.labelSaturday.Size = new System.Drawing.Size(65, 17);
+            this.labelSaturday.TabIndex = 3;
+            this.labelSaturday.Text = "Saturday";
+            // 
+            // listBoxSaturday
+            // 
+            this.listBoxSaturday.FormattingEnabled = true;
+            this.listBoxSaturday.ItemHeight = 16;
+            this.listBoxSaturday.Location = new System.Drawing.Point(166, 373);
+            this.listBoxSaturday.Name = "listBoxSaturday";
+            this.listBoxSaturday.Size = new System.Drawing.Size(120, 84);
+            this.listBoxSaturday.TabIndex = 2;
+            // 
+            // labelFriday
+            // 
+            this.labelFriday.AutoSize = true;
+            this.labelFriday.Location = new System.Drawing.Point(163, 225);
+            this.labelFriday.Name = "labelFriday";
+            this.labelFriday.Size = new System.Drawing.Size(47, 17);
+            this.labelFriday.TabIndex = 5;
+            this.labelFriday.Text = "Friday";
+            // 
+            // listBoxFriday
+            // 
+            this.listBoxFriday.FormattingEnabled = true;
+            this.listBoxFriday.ItemHeight = 16;
+            this.listBoxFriday.Location = new System.Drawing.Point(162, 251);
+            this.listBoxFriday.Name = "listBoxFriday";
+            this.listBoxFriday.Size = new System.Drawing.Size(120, 84);
+            this.listBoxFriday.TabIndex = 4;
+            // 
+            // labelThurday
+            // 
+            this.labelThurday.AutoSize = true;
+            this.labelThurday.Location = new System.Drawing.Point(163, 93);
+            this.labelThurday.Name = "labelThurday";
+            this.labelThurday.Size = new System.Drawing.Size(68, 17);
+            this.labelThurday.TabIndex = 7;
+            this.labelThurday.Text = "Thursday";
+            // 
+            // listBoxThurday
+            // 
+            this.listBoxThurday.FormattingEnabled = true;
+            this.listBoxThurday.ItemHeight = 16;
+            this.listBoxThurday.Location = new System.Drawing.Point(162, 119);
+            this.listBoxThurday.Name = "listBoxThurday";
+            this.listBoxThurday.Size = new System.Drawing.Size(120, 84);
+            this.listBoxThurday.TabIndex = 6;
+            // 
+            // labelWednesday
+            // 
+            this.labelWednesday.AutoSize = true;
+            this.labelWednesday.Location = new System.Drawing.Point(11, 382);
+            this.labelWednesday.Name = "labelWednesday";
+            this.labelWednesday.Size = new System.Drawing.Size(83, 17);
+            this.labelWednesday.TabIndex = 9;
+            this.labelWednesday.Text = "Wednesday";
+            // 
+            // listBoxWednesday
+            // 
+            this.listBoxWednesday.FormattingEnabled = true;
+            this.listBoxWednesday.ItemHeight = 16;
+            this.listBoxWednesday.Location = new System.Drawing.Point(10, 408);
+            this.listBoxWednesday.Name = "listBoxWednesday";
+            this.listBoxWednesday.Size = new System.Drawing.Size(120, 84);
+            this.listBoxWednesday.TabIndex = 8;
+            // 
+            // labelTuesday
+            // 
+            this.labelTuesday.AutoSize = true;
+            this.labelTuesday.Location = new System.Drawing.Point(9, 267);
+            this.labelTuesday.Name = "labelTuesday";
+            this.labelTuesday.Size = new System.Drawing.Size(63, 17);
+            this.labelTuesday.TabIndex = 11;
+            this.labelTuesday.Text = "Tuesday";
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.ItemHeight = 16;
+            this.listBox5.Location = new System.Drawing.Point(8, 293);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(120, 84);
+            this.listBox5.TabIndex = 10;
+            // 
+            // labelMonday
+            // 
+            this.labelMonday.AutoSize = true;
+            this.labelMonday.Location = new System.Drawing.Point(7, 154);
+            this.labelMonday.Name = "labelMonday";
+            this.labelMonday.Size = new System.Drawing.Size(58, 17);
+            this.labelMonday.TabIndex = 13;
+            this.labelMonday.Text = "Monday";
+            // 
+            // listBoxMonday
+            // 
+            this.listBoxMonday.FormattingEnabled = true;
+            this.listBoxMonday.ItemHeight = 16;
+            this.listBoxMonday.Location = new System.Drawing.Point(6, 180);
+            this.listBoxMonday.Name = "listBoxMonday";
+            this.listBoxMonday.Size = new System.Drawing.Size(120, 84);
+            this.listBoxMonday.TabIndex = 12;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1315, 516);
+            this.Controls.Add(this.groupBoxWeekMenu);
             this.Controls.Add(this.groupBoxDishes);
             this.Name = "Main";
             this.Text = "PlanShopEat";
             this.Load += new System.EventHandler(this.Main_Load);
             this.groupBoxDishes.ResumeLayout(false);
             this.groupBoxDishes.PerformLayout();
+            this.groupBoxWeekMenu.ResumeLayout(false);
+            this.groupBoxWeekMenu.PerformLayout();
             this.ResumeLayout(false);
 
       }
@@ -125,6 +293,21 @@
         private System.Windows.Forms.Label labelCategory;
         private System.Windows.Forms.ComboBox comboBoxCategory;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBoxWeekMenu;
+        private System.Windows.Forms.Label labelMonday;
+        private System.Windows.Forms.ListBox listBoxMonday;
+        private System.Windows.Forms.Label labelTuesday;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.Label labelWednesday;
+        private System.Windows.Forms.ListBox listBoxWednesday;
+        private System.Windows.Forms.Label labelThurday;
+        private System.Windows.Forms.ListBox listBoxThurday;
+        private System.Windows.Forms.Label labelFriday;
+        private System.Windows.Forms.ListBox listBoxFriday;
+        private System.Windows.Forms.Label labelSaturday;
+        private System.Windows.Forms.ListBox listBoxSaturday;
+        private System.Windows.Forms.Label labelSunday;
+        private System.Windows.Forms.ListBox listBoxSunday;
     }
 }
 
